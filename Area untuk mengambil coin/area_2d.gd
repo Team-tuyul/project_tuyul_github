be45476d1b75b9d800c3cfe7174e525f193566f2
Mario Pratama <mario.pratama2016@gmail.com>
2025-11-26 00:09:16 +0800

    fix 1.2

--- Area untuk mengambil coin/area_2d.gd
+++ Area untuk mengambil coin/area_2d.gd
@@ -1,21 +1,28 @@
 extends Area2D
 
 @export var item_name: String = "sword"
+@onready var sfx = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	self.body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
-	# Pastikan yang menyentuh punya group 'player'
 	if body.is_in_group("player"):
+		
 		match item_name:
 			"sword":
-				# ⚡️ Panggil fungsi di Player yang akan memicu pergantian Scene
 				if body.has_method("ambil_pedang"):
 					body.ambil_pedang()
-				
+
 			"coin":
 				if body.has_method("tambah_koin"):
 					body.tambah_koin(1)
-		
-		queue_free() # Hilangkan item dari dunia setelah diambil
+
+		# 🔊 Mainkan suara pickup jika ada
+		if sfx.stream != null:
+			sfx.play()
+			# Hapus item setelah suara selesai
+			await sfx.finished
+			queue_free()
+		else:
+			queue_free()
